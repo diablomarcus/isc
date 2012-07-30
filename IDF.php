2af48149d595d2395857b92b887ab81ad2554c16
diablomarcus <diablomarcus@gmail.com>
2012-07-29 21:55:49 -0500

    Refactored a bit more code out into table_head and added info to contact page

--- IDF.php
+++ IDF.php
@@ -1,18 +1,5 @@
 <?php include("header.php"); ?>
-<body>
-<div align="center">
-  <table width="780" height="119" border="0" cellpadding="0" cellspacing="0">
-    <tr>
-      <td><img src="images/image_-_banner_-_ISC_main.jpg" width="780" height="100" hspace="0" vspace="0"><br>
-      <img src="images/styling/image_-_bar_-_blue_001.jpg" width="780" height="15" hspace="0" vspace="0" align="top"></td>
-    </tr>
-  </table>
-  <table width="780" height="224" border="0" cellpadding="0" cellspacing="0" bgcolor="#000000">
-    <tr>
-      <td height="15" valign="top" background="images/styling/bg/image_-_bg_-_table_shadow_-_top_001.gif"><div align="center" class="style1"><a href="index.php">home</a> :: <a href="industry.php">industrial operations</a> :: defense :: <a href="contact.php">contact</a></div></td>
-    </tr>
-    <tr>
-      <td height="181" align="left" valign="top" background="images/styling/bg/image_-_bg_-_table_001.gif"><div align="center">
+<?php include("table_head.php"); ?>
         <br>
         <table width="700" height="148" border="0" cellpadding="0" cellspacing="0">
           <tr valign="top">
