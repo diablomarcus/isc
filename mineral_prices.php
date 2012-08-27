0b778b50a67350ace585f436978d9bcd3e890e32
diablomarcus <diablomarcus@gmail.com>
2012-08-26 20:10:17 -0500

    Halfway state to functioning price calculator

--- mineral_prices.php
+++ mineral_prices.php
@@ -1,18 +1,34 @@
 <?php include("header.php"); ?>
 <?php include("table_head.php"); ?>
         <br>
+        <script type="text/javascript">
+        </script>
+
         <table width="700" height="148" border="0" cellpadding="0" cellspacing="0">
           <tr valign="top">
-            <td width="345" align="left">
-            <iframe id='google_doc' width='362' scrolling='no' height='1250' style="background-color:transparent" allowtransparency="true" frameborder='0' src='https://docs.google.com/spreadsheet/pub?key=0AncM79JXO4EWdE9uZElSNHFJRmw2ajVSMnFUOVF5MEE&single=true&gid=2&range=a1%3Ac65&output=html&widget=false'></iframe>
+            <td align="left">
+              <table id="price_table">
+                <?php
+                  $current_prices = file_get_contents("https://docs.google.com/spreadsheet/pub?key=0AncM79JXO4EWdE9uZElSNHFJRmw2ajVSMnFUOVF5MEE&single=true&gid=2&output=txt");
+                  $prices=explode('-', $current_prices);
+                  for ($i=1; $i<(sizeof($prices)-1); $i++) {
+                     $value=trim($prices[$i]);
+                     $item=trim(strstr($value, "\t", true));
+                     $price=trim(strstr($value, "\t"));
+                     if ($price!=''){
+                     echo '<tr><td class="item_label">',"$item ($price)",'</td></tr>';
+                     }else{ echo '<tr><td></td></tr>'; }
+                  }
+                ?>
+              </table>
             </td>
-            <td width="10">&nbsp;</td>
+            <td id="dumb" width="10">&nbsp;</td>
             <td width="345" align="left">
               <p>&nbsp;</p>
               <p>&nbsp;</p>
               <p>&nbsp;</p>
               <p>&nbsp;</p>
-              <p align="center"><img src="images/image_-_logo_-_ISC_grayscale_-_S.gif" width="150" height="150"></p></td>
+              <p align="center"><img src="images/image_-_logo_-_ISC_grayscale_-_S.gif" width="152" height="150"></p></td>
           </tr>
         </table>
         <br>
